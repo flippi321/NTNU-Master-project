046fc245e806d16e6bf7f825fa3e0a09457c79e9
Christoffer Brevik <chribrev@stud.ntnu.no>
2026-02-02 14:02:40 +0100

    Added a ssim loss function This should work for 3D

---
+++ utils/loss_functions.py
@@ -0,0 +1,4 @@
+from pytorch_msssim import ssim
+
+def ssim_loss(predicted, target, win_size=11, win_sigma=1.5, K=(0.01, 0.03)):
+    return 1 - ssim(predicted, target, win_size=win_size, win_sigma=win_sigma, K=K, data_range=1.0)
