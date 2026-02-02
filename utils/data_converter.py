5c772284eb9b0e8727b25a06da5e3d30b799c023
Christoffer Brevik <chribrev@stud.ntnu.no>
2026-02-02 15:02:19 +0100

    Updated all files converting to tensor to have the 5 dimentions reqired (xyz + channel and batch)

--- utils/data_converter.py
+++ utils/data_converter.py
@@ -15,12 +15,12 @@ class DataConverter():
         data = img.get_fdata()
         return data
     
-    def load_path_as_tensor(self, path):
+    def load_path_as_tensor(self, path, device = 'cuda'):
         """
         Function to load a NIfTI volume as a torch tensor using the data path
         """
         data = self.load_path_as_numpy(path)
-        tensor = torch.tensor(data, dtype=torch.float32).unsqueeze(0)  # TODO: Sjekk om vi kan fjerne unsqueeze
+        tensor = torch.tensor(data, dtype=torch.float32, device=device).unsqueeze(0).unsqueeze(0)  # Get correct (1, 1, D, H, W) structure
         return tensor.float()
     
     def get_middle_slice_from_path(self, data_path):
@@ -31,13 +31,13 @@ class DataConverter():
         data = self.load_path_as_numpy(data_path)
         return data[:, :, index]
     
-    def get_all_slices_as_tensor(self, data_path):
+    def get_all_slices_as_tensor(self, data_path, device = 'cuda'):
         data = self.load_path_as_numpy(data_path)
-        return [torch.tensor(slice, dtype=torch.float32) for slice in data.transpose(2, 0, 1)]
+        return [torch.tensor(slice, dtype=torch.float32, device=device).unsqueeze(0).unsqueeze(0) for slice in data.transpose(2, 0, 1)]
 
     # ----- Convert between Numpy and Tensor -----
     def numpy_to_tensor(self, array: np.ndarray, device = 'cuda'):
-        tensor = torch.tensor(array, dtype=torch.float32, device=device).unsqueeze(0)  # TODO: Sjekk om vi kan fjerne unsqueeze
+        tensor = torch.tensor(array, dtype=torch.float32, device=device).unsqueeze(0).unsqueeze(0)  # Get correct (1, 1, D, H, W) structure
         return tensor.float()
     
     def tensor_to_numpy(self, tensor: torch.Tensor):
