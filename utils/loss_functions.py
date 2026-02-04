376e26e9366c95178b3804c15d9b5a49c523a69f
Christoffer Brevik <chribrev@stud.ntnu.no>
2026-02-04 10:37:12 +0100

    Added a basic L1 loss

--- utils/loss_functions.py
+++ utils/loss_functions.py
@@ -1,4 +1,8 @@
 from pytorch_msssim import ssim
+import torch
 
 def ssim_loss(predicted, target, win_size=11, win_sigma=1.5, K=(0.01, 0.03)):
     return 1 - ssim(predicted, target, win_size=win_size, win_sigma=win_sigma, K=K, data_range=1.0)
+
+def l1_loss(predicted, target):
+    return torch.mean(torch.abs(predicted - target))
