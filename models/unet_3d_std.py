04428759705e13791ac4778c46e69de7713d0102
Christoffer Brevik <chribrev@stud.ntnu.no>
2026-02-04 10:09:46 +0100

    Fixed the skip-layer issue where down and up layers had different dimentions

--- models/unet_3d_std.py
+++ models/unet_3d_std.py
@@ -15,28 +15,30 @@ class ConvBlock(nn.Module):
             nn.InstanceNorm3d(out_ch),
             nn.LeakyReLU(0.1, inplace=True),
         )
-
     def forward(self, x): return self.block(x)
 
 class Down(nn.Module):
     def __init__(self, in_ch, out_ch):
         super().__init__()
-        # Downsample with strided conv
-        self.pool = nn.Conv3d(in_ch, in_ch, kernel_size=2, stride=2, groups=in_ch, bias=False)
         self.conv = ConvBlock(in_ch, out_ch)
+        self.pool = nn.Conv3d(out_ch, out_ch, kernel_size=2, stride=2, groups=out_ch, bias=False)
 
-    def forward(self, x): return self.conv(self.pool(x))
+    def forward(self, x):
+        skip = self.conv(x)
+        down = self.pool(skip)
+        return skip, down
 
 class Up(nn.Module):
-    def __init__(self, in_ch, out_ch):
+    def __init__(self, in_ch, out_ch, do_upsample: bool = True):
         super().__init__()
-        # Upsample with trilinear + conv
+        self.do_upsample = do_upsample
         self.up = nn.Upsample(scale_factor=2, mode="trilinear", align_corners=False)
         self.reduce = nn.Conv3d(in_ch, out_ch, kernel_size=1)
         self.conv = ConvBlock(out_ch * 2, out_ch)
 
     def forward(self, x, skip):
-        x = self.up(x)
+        if self.do_upsample:
+            x = self.up(x)
         x = self.reduce(x)
 
         # Skip connection concat
@@ -64,21 +66,21 @@ class UNet3D(nn.Module):
         self.u4 = Up(base * 16, base * 8)
         self.u3 = Up(base * 8, base * 4)
         self.u2 = Up(base * 4, base * 2)
-        self.u1 = Up(base * 2, base)
+        self.u1 = Up(base * 2, base, do_upsample=False)   # On the last layer we don't need to upsample anymore
 
         self.out = nn.Conv3d(base, out_ch, kernel_size=1)
 
     def forward(self, x):
         s1 = self.e1(x)
-        s2 = self.e2(s1)
-        s3 = self.e3(s2)
-        s4 = self.e4(s3)
+        s2, x2 = self.e2(s1)
+        s3, x3 = self.e3(x2)
+        s4, x4 = self.e4(x3)
 
-        b = self.bott_sec(self.bott(s4))
+        b = self.bott_sec(self.bott(x4))
 
         d4 = self.u4(b, s4)
         d3 = self.u3(d4, s3)
         d2 = self.u2(d3, s2)
         d1 = self.u1(d2, s1)
-
+        
         return self.out(d1)
