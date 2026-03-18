bf567287b3b2143cef5879222a6ec97a6fdb0e0e
Christoffer Brevik <chribrev@stud.ntnu.no>
2026-03-18 11:03:47 +0100

    Added function to convert a volume into a nib.gz file

--- utils/data_converter.py
+++ utils/data_converter.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import numpy as np
 import nibabel as nib
@@ -15,6 +16,18 @@ class DataConverter():
         img = nib.load(path)
         data = img.get_fdata()
         return data
+    
+    def numpy_to_nib_gz(self, volume: np.ndarray, out_path: str):
+        """
+        Function to transform a 3D volume to a nii.gz file
+        """
+        affine = np.eye(4)
+
+        img = nib.Nifti1Image(volume, affine)
+        nib.save(img, str(out_path))
+
+        return os.path.exists(out_path)
+    
     
     def load_path_as_tensor(self, path: str, device = 'cuda'):
         """
@@ -43,7 +56,7 @@ class DataConverter():
     
     def tensor_to_numpy(self, tensor: torch.Tensor):
         # TODO Sjekk at funker
-        return tensor.detach().cpu().numpy()
+        return tensor.detach().cpu().squeeze().numpy()
     
     # TODO Make not hardcoded
     def get_patient_feature_vector(self, data_path: str, usage_list=None) -> torch.Tensor:
